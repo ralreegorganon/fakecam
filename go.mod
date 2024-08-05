9a00fd3e2032e38d18355c6224286c3e9e891e96
Jason Jones <jasonedwardjones@gmail.com>
2024-08-05 15:56:34 -0800

    Add route serving outdated image

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/ralreegorganon/fakecam
 
-go 1.20
+go 1.22
--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 var port = flag.String("port", "32778", "TCP port to bind to")
 var directory = flag.String("directory", "images", "directory containing images")
-var path = flag.String("path", "photo", "path to serve image on")
 
 func main() {
 	flag.Parse()
@@ -21,14 +21,15 @@ func main() {
 		panic(err)
 	}
 
-	log.Printf("Serving images from %v at http://localhost:%s/%s", d, *port, *path)
+	log.Printf("Serving images from %v at http://localhost:%s/photo", d, *port)
 
-	handler := http.HandlerFunc(handleRequest)
-	http.Handle("/"+*path, handler)
-	http.ListenAndServe(":"+*port, nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/photo", getPhoto)
+	mux.HandleFunc("/outdated", getOutdated)
+	http.ListenAndServe(":"+*port, mux)
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
+func getPhoto(w http.ResponseWriter, r *http.Request) {
 	path, err := filepath.Abs(*directory)
 	if err != nil {
 		panic(err)
@@ -44,5 +45,28 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(fileBytes)
+}
+
+func getOutdated(w http.ResponseWriter, r *http.Request) {
+	path, err := filepath.Abs(*directory)
+	if err != nil {
+		panic(err)
+	}
+	files, err := os.ReadDir(*directory)
+	if err != nil {
+		panic(err)
+	}
+	file := files[rand.Intn(len(files))]
+	fileBytes, err := os.ReadFile(filepath.Join(path, file.Name()))
+	if err != nil {
+		panic(err)
+	}
+	// Create a last modified date in the past, so that "helpful" browsers will
+	// serve the image from the cache.
+	thePast := time.Now().Add(-15 * time.Minute).UTC().Format(http.TimeFormat)
+	w.Header().Set("Last-Modified", thePast)
+	w.Header().Set("Content-Type", "image/jpeg")
+	w.WriteHeader(http.StatusOK)
 	w.Write(fileBytes)
 }
